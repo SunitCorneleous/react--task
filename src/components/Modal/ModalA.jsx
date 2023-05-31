8da5e6ed16fd8a3fa52f07c40e7ce3407c9b90c1
Sunit Corneleous <sunit.corneleous25@gmail.com>
2023-06-01 01:55:15 +0600

    showing data in modal a

--- src/components/Modal/ModalA.jsx
+++ src/components/Modal/ModalA.jsx
@@ -1,7 +1,23 @@
-import React from 'react';
-import { Button, Modal } from 'react-bootstrap';
+import React, { useEffect, useState } from 'react';
+import { Button, Modal, Table } from 'react-bootstrap';
 
 const ModalA = ({ show, handleClose, setModal }) => {
+  const [contacts, setContacts] = useState([]);
+
+  useEffect(() => {
+    const getContacts = async () => {
+      const res = await fetch(
+        'https://contact.mediusware.com/api/contacts/?page_size=5'
+      );
+
+      const data = await res.json();
+
+      setContacts(data.results);
+    };
+
+    getContacts();
+  }, []);
+
   return (
     <Modal
       show={show}
@@ -14,8 +30,24 @@ const ModalA = ({ show, handleClose, setModal }) => {
         <Modal.Title>Modal A</Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        I will not close if you click outside me. Don not even try to press
-        escape key.
+        <Table striped bordered hover size='sm'>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Country</th>
+              <th>Phone</th>
+            </tr>
+          </thead>
+          <tbody>
+            {contacts?.map(item => (
+              <tr key={item.id}>
+                <td>{item.id}</td>
+                <td>{item?.country.name}</td>
+                <td>@{item.phone}</td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
       </Modal.Body>
       <Modal.Footer>
         <Button variant='primary' onClick={() => setModal('A')}>
