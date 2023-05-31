291232ba67ec8a3472d233b24a6441c1140345f6
Sunit Corneleous <sunit.corneleous25@gmail.com>
2023-05-31 23:24:48 +0600

    name and status showing in table

--- src/components/Problem-1.jsx
+++ src/components/Problem-1.jsx
@@ -1,59 +1,107 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
 const Problem1 = () => {
+  const [users, setUsers] = useState([]);
+  const [show, setShow] = useState('all');
 
-    const [show, setShow] = useState('all');
+  const handleClick = val => {
+    setShow(val);
+  };
 
-    const handleClick = (val) =>{
-        setShow(val);
-    }
+  const handleAdd = e => {
+    e.preventDefault();
 
-    return (
+    const form = e.target;
 
-        <div className="container">
-            <div className="row justify-content-center mt-5">
-                <h4 className='text-center text-uppercase mb-5'>Problem-1</h4>
-                <div className="col-6 ">
-                    <form className="row gy-2 gx-3 align-items-center mb-4">
-                        <div className="col-auto">
-                            <input type="text" className="form-control" placeholder="Name"/>
-                        </div>
-                        <div className="col-auto">
-                            <input type="text" className="form-control" placeholder="Status"/>
-                        </div>
-                        <div className="col-auto">
-                            <button type="submit" className="btn btn-primary">Submit</button>
-                        </div>
-                    </form>
-                </div>
-                <div className="col-8">
-                    <ul className="nav nav-pills mb-3" id="pills-tab" role="tablist">
-                        <li className="nav-item">
-                            <button  className={`nav-link ${show==='all' && 'active'}`} type="button" onClick={()=>handleClick('all')}>All</button>
-                        </li>
-                        <li className="nav-item">
-                            <button className={`nav-link ${show==='active' && 'active'}`} type="button" onClick={()=>handleClick('active')}>Active</button>
-                        </li>
-                        <li className="nav-item">
-                            <button  className={`nav-link ${show==='completed' && 'active'}`} type="button" onClick={()=>handleClick('completed')}>Completed</button>
-                        </li>
-                    </ul>
-                    <div className="tab-content"></div>
-                    <table className="table table-striped ">
-                        <thead>
-                        <tr>
-                            <th scope="col">Name</th>
-                            <th scope="col">Status</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        
-                        </tbody>
-                    </table>
-                </div>
+    const name = form.name.value;
+    const status = form.status.value;
+
+    setUsers([...users, { name, status }]);
+  };
+
+  return (
+    <div className='container'>
+      <div className='row justify-content-center mt-5'>
+        <h4 className='text-center text-uppercase mb-5'>Problem-1</h4>
+        <div className='col-6 '>
+          <form
+            className='row gy-2 gx-3 align-items-center mb-4'
+            onSubmit={handleAdd}
+          >
+            <div className='col-auto'>
+              <input
+                type='text'
+                className='form-control'
+                placeholder='Name'
+                name='name'
+              />
             </div>
+            <div className='col-auto'>
+              <input
+                type='text'
+                className='form-control'
+                placeholder='Status'
+                name='status'
+              />
+            </div>
+            <div className='col-auto'>
+              <button type='submit' className='btn btn-primary'>
+                Submit
+              </button>
+            </div>
+          </form>
         </div>
-    );
+        <div className='col-8'>
+          <ul className='nav nav-pills mb-3' id='pills-tab' role='tablist'>
+            <li className='nav-item'>
+              <button
+                className={`nav-link ${show === 'all' && 'active'}`}
+                type='button'
+                onClick={() => handleClick('all')}
+              >
+                All
+              </button>
+            </li>
+            <li className='nav-item'>
+              <button
+                className={`nav-link ${show === 'active' && 'active'}`}
+                type='button'
+                onClick={() => handleClick('active')}
+              >
+                Active
+              </button>
+            </li>
+            <li className='nav-item'>
+              <button
+                className={`nav-link ${show === 'completed' && 'active'}`}
+                type='button'
+                onClick={() => handleClick('completed')}
+              >
+                Completed
+              </button>
+            </li>
+          </ul>
+          <div className='tab-content'></div>
+          <table className='table table-striped '>
+            <thead>
+              <tr>
+                <th scope='col'>Name</th>
+                <th scope='col'>Status</th>
+              </tr>
+            </thead>
+            <tbody>
+              {users?.map(user => (
+                <tr>
+                  <td>{user.name}</td>
+                  <td>{user.status}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Problem1;
